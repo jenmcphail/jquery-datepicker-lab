993db0b97293d0e00916a953c33ca287a224ac71
jenmcphail <mcphail.jen@gmail.com>
2016-05-10 20:16:33 -0700

    step-4 homework

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ function daysLeft() {
 // We need a variable where we can store the integer value difference of days and display that in a concatenated HTML string.
 // "You have ____ days left!"
 
+var daysLeftDisplayMessage = "You have" + diffDays + " days left!"
+
 }
 
 $("#datepicker").on("change", function(){
@@ -26,9 +28,6 @@ $("#datepicker").on("change", function(){
 // 14. Now, instead of console.log, let's update the HTML string every time the user clicks a date.
 // Use jQuery to write the result string in the element with an id of 'result'
 
-// _____
-
-});
 
 // UH-OH! We defined the variable inside daysLeft and we need the same data in our .on('change', function(){})
 // 15. Define the variable globally and leave your local variable references alone.
