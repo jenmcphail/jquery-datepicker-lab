f982f919fbd0495192c1187bb9ea15fb2c9b1040
jenmcphail <mcphail.jen@gmail.com>
2016-05-10 20:10:37 -0700

    step-3 homework

--- script.js
+++ script.js
@@ -24,27 +24,39 @@ function daysLeft() {
 $("#datepicker").on("change", function(a){
 	daysLeft();
 	console.log(a);
-});
+
 
 
 
 // 9. For the second variable, you need to call getTime on your today variable
 
+var currentTime = $(today).getTime;
+
 // 10. Now we need a variable to store the difference in days between today and the end date selected by the user.
 // Basically, we need to subtract b from a
+
+b = daysLeft() - currentTime;
 
 
 // 11. SPOILER ALERT!  If you've done it correctly, you will get a difference in milliseconds, which is not what we want.
 // ...so we need one more variable to store a constant and we need to define it before we store the difference in days.
 // That makes difference in days our FOURTH variable, so let's call our THIRD variable c. 
 // c = hours per day * minutes per hour * seconds per minute * ms per sec
+
+c = 24*60*60*1000;
+
 // Do the math and store the value.
 
 // If all is well, you can console.log your difference in days variable and it will change depending on what date is clicked on the datepicker.
+
+var daysDifference = Math.round((a-b)/c);
 
 // 12. There is still a problem. We need the result to be an integer.
 // Here is a link to the Math object:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 // I bet you can figure out which method to call on your difference in days variable.
 // It's one of the ones listed under Methods in the left column of the page linked above.
+console.log(daysDifference);
+});
+
 
 
